e74c607b21e681fb01a58e632fd140b1221e3675
Travis CI User <travis@example.org>
2019-02-14 19:51:44 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,7 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["byteorder"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["generic_array"] = {"name":"","dirs":[],"files":["arr.rs","functional.rs","hex.rs","impls.rs","iter.rs","lib.rs","sequence.rs"]};
+sourcesIndex["hash32"] = {"name":"","dirs":[],"files":["fnv.rs","lib.rs","murmur3.rs"]};
+sourcesIndex["heapless"] = {"name":"","dirs":[{"name":"spsc","dirs":[],"files":["mod.rs","split.rs"]}],"files":["__core.rs","binary_heap.rs","cfail.rs","const_fn.rs","indexmap.rs","indexset.rs","lib.rs","linear_map.rs","sealed.rs","string.rs","vec.rs"]};
+sourcesIndex["typenum"] = {"name":"","dirs":[],"files":["array.rs","bit.rs","int.rs","lib.rs","marker_traits.rs","operator_aliases.rs","private.rs","type_operators.rs","uint.rs"]};
+createSourceSidebar();
